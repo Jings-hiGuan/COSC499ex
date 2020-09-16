183bffb8cd5487876def85100f511d08e273086f
kuaitule <51555228+kuaitule@users.noreply.github.com>
2020-09-16 03:50:03 -0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        int[] array = {2,6,3,7,8,9,1,16,21,239,24,124};  
+        System.out.println("Before Insertion Sort");    
+        for(int i:array){    
+            System.out.print(i+" ");    
+        }    
+        System.out.println();  
+    }
+}
